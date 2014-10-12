bdbbc6111e80e5acec63b6c700a5c88dfc9de5c8
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2014-10-12 13:21:58 +0900

    日付指定で実績一覧を取得する機能を追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,8 @@ class Task < ActiveRecord::Base
   def time_second
     end_at - start_at
   end
+
+  def self.by_start_date(date)
+    where("start_at LIKE ?", "#{date}%").order(:start_at)
+  end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -7,4 +7,18 @@ describe Task do
       end_at: "2014/10/10 10:10:20")
     expect(task.time_second).to eq(10)
   end
+
+  it "returns tasks with start date" do
+    task1 = Task.new(
+      start_at: "2014/10/10 10:10:10",
+      end_at: "2014/10/11 10:10:20")
+    task2 = Task.new(
+      start_at: "2014/10/10 10:10:10",
+      end_at: "2014/10/10 10:10:20")
+    task3 = Task.new(
+      start_at: "2014/10/9 10:10:10",
+      end_at: "2014/10/10 10:10:20")
+
+    expect(Task.by_start_date("2014/10/10")).to eq([task1, task2])
+  end
 end
