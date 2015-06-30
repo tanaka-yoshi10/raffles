91c481a100b32dc4e274829e4d1af55992ccf85d
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2015-07-01 07:46:10 +0900

    apply bootstrap

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,5 +1,5 @@
 module ProjectsHelper
   def projects_select_tag(f)
-    f.collection_select(:project_id, Project.order(:category, :code).all, :id, :code_and_name)
+    f.collection_select(:project_id, Project.order(:category, :code).all, :id, :code_and_name, {} ,{:class => "form-control"})
   end
 end
