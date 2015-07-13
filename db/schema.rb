9cd35a3a9c9cd3d911f2485e1f75b2a050936896
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2015-07-14 00:54:19 +0900

    add user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141013105628) do
+ActiveRecord::Schema.define(version: 20150713151853) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
@@ -31,5 +31,12 @@ ActiveRecord::Schema.define(version: 20141013105628) do
   end
 
   add_index "tasks", ["project_id"], name: "index_tasks_on_project_id"
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
