4e30dc0834c9b5842c833b82fdf7052905bf79f4
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2015-07-31 07:56:17 +0900

    completed projectは選択肢に表示しないようにする

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,5 +1,5 @@
 module ProjectsHelper
   def projects_select_tag(f)
-    f.collection_select(:project_id, Project.order(:category, :code).all, :id, :code_and_name, {} ,{:class => "form-control"})
+    f.collection_select(:project_id, Project.order(:category, :code).where(completed: false).all, :id, :code_and_name, {} ,{:class => "form-control"})
   end
 end
