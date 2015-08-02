232a75986de8a9e896019592c5d53a15aad8ce4f
Yoshito Tanaka <tanaka.yoshi10@gmail.com>
2015-08-02 15:55:06 +0900

    add checkbox to project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -70,6 +70,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:name, :code, :category)
+      params.require(:project).permit(:name, :code, :category, :completed)
     end
 end
